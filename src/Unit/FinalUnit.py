76d598b10f7116d96e37375bf3e9021ee5d0aca4
yhswjtuilmare <yh_swjtu@163.com>
2018-03-11 14:27:19 +0800

    update at March 12th

--- src/Unit/FinalUnit.py
+++ src/Unit/FinalUnit.py
@@ -7,6 +7,10 @@ import shelve
 import Util.DataUtil as DataUtil
 import lib.DecisionTreeLib as DTLib
 import lib.RFLib as RFLib
+import lib.LogisticLib as LRLib
+import csv
+
+filePath = r"G:\研究生课件\数据挖掘\实验数据"
 
 def testDTModel(filename="bank-additional"):
     db = shelve.open("MiningModel")
@@ -36,6 +40,36 @@ def testRFModel(filename="bank-additional"):
     errorRatio = error / len(dataSet)
     print("RF: error ratio is %.3f, correct ratio is %.3f" % (errorRatio, 1 - errorRatio))
 
+def loaddata_temp(filename):
+    try:
+        fp = open("{0}/{1}.csv".format(filePath, filename), "r")
+        reader = csv.reader(fp)
+        trainSet = []
+        trainLabel = []
+        for line in reader:
+            trainSet.append(line[0: -1])
+            trainLabel.append(int(line[-1]))
+        return trainSet, trainLabel
+    except Exception as e:
+        print(e)
+    finally:
+        fp.close()
+
+def testLRModel():
+    db = shelve.open("MiningModel")
+    maxCorrectRatio = db["LRModelCorrectRatio"]
+    weight = db["LRModel"]
+    db.close()
+    dataSet, labelSet = loaddata_temp("bank-addtional-format-lr")
+    error = 0
+    for data, label in zip(dataSet, labelSet):
+        predict_label = LRLib.classifyVector(data, weight)
+        if predict_label != label:
+            error += 1
+    errorRatio = error / len(dataSet)
+    print("LR: error ratio is %.3f, correct ratio is %.3f" % (errorRatio, 1 - errorRatio))
+
 if __name__ == "__main__":
     testDTModel()
+    testLRModel()
     testRFModel()
--- src/Unit/GeneralUnit.py
+++ src/Unit/GeneralUnit.py
@@ -7,8 +7,12 @@ import shelve
 import Util.DataUtil as DataUtil
 import lib.DecisionTreeLib as DTLib
 import lib.RFLib as RFLib
+import lib.LogisticLib as LRLib
+import csv
 import time
 import numpy as np
+
+filePath = r"G:\研究生课件\数据挖掘\实验数据"
 
 def loadDataSet(filename):
     print("Loading data...")
@@ -61,5 +65,34 @@ def serializeRFModel():
     db["RFModelCorrectRatio"] = maxRatio
     db.close()
 
+def loaddata_temp(filename):
+    try:
+        fp = open("{0}/{1}.csv".format(filePath, filename), "r")
+        reader = csv.reader(fp)
+        trainSet = []
+        trainLabel = []
+        for line in reader:
+            trainSet.append(line[0: -1])
+            trainLabel.append(int(line[-1]))
+        return trainSet, trainLabel
+    except Exception as e:
+        print(e)
+    finally:
+        fp.close()
+
 if __name__ == "__main__":
-    pass
+    start = time.clock()
+    dataSet, labelSet = loaddata_temp("bank-addtional-format-lr")
+    trainSet, trainLabel, testSet, testLabel = DataUtil.generateTrainSet(dataSet, labelSet)
+    weight, logList = LRLib.stocGradDescent(trainSet, trainLabel)
+    errorCount = 0
+    for data, label in zip(testSet, testLabel):
+        predict_label = LRLib.classifyVector(data, weight)
+        if predict_label != label:
+            errorCount += 1
+    ratio = errorCount / len(testLabel)
+    print("the error ratio is %.3f, the correct ratio is %.3f -- %.3fs" % (ratio, 1 - ratio, time.clock() - start))
+    db = shelve.open("MiningModel")
+    db["LRModel"] = weight
+    db["LRModelCorrectRatio"] = 1 - ratio
+    db.close()
